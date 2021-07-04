67542e600226f32c04c96a77ca3e42a8d58f3e32
kkjsw17 <kkjsw17@naver.com>
2021-07-04 17:08:33 +0900

    done

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import sys
 import cv2
+import numpy as np
 from processor import Processor
 from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QFileDialog, QMessageBox
 
 
-class MyApp(QMainWindow):
+class Window(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
@@ -18,19 +19,24 @@ class MyApp(QMainWindow):
         self.show()
 
     def initMenuBar(self):
-        videoLoad = QAction('Open', self)
-        videoLoad.setShortcut('Ctrl+O')
-        videoLoad.setStatusTip('Open Video File')
-        videoLoad.triggered.connect(self.loadVideo)
+        detectSkinAction = QAction('Skin Detection', self)
+        detectSkinAction.setShortcut('Ctrl+S')
+        detectSkinAction.setStatusTip('Load Video for Detect Skin')
+        detectSkinAction.triggered.connect(self.detectSkin)
+
+        detectVehicleAction = QAction('Vehicle Detection', self)
+        detectVehicleAction.setShortcut('Ctrl+V')
+        detectVehicleAction.setStatusTip('Load Video for Detect Vehicle')
+        detectVehicleAction.triggered.connect(self.detectVehicle)
 
         self.statusBar()
 
         menubar = self.menuBar()
         menubar.setNativeMenuBar(False)
-        filemenu = menubar.addMenu('&File')
-        filemenu.addAction(videoLoad)
+        menubar.addAction(detectSkinAction)
+        menubar.addAction(detectVehicleAction)
 
-    def loadVideo(self):
+    def detectSkin(self):
         fileName = QFileDialog.getOpenFileName(self, 'Open File', '/', filter='Videos (*.avi *.mov *.mp4)')
 
         if fileName[0]:
@@ -39,7 +45,7 @@ class MyApp(QMainWindow):
             if cap.isOpened():
                 frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
                 frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                frameRate = 30
+                frameRate = 10
 
                 # u = np.array([152, 106])
                 # B = 2.105 * 255
@@ -53,27 +59,111 @@ class MyApp(QMainWindow):
                     if not retval:
                         break
 
-                    prc = Processor(frame)
+                    # frame = cv2.GaussianBlur(frame, (0, 0), 1.2)
+                    filtered_frame = cv2.bilateralFilter(frame, 10, 50, 50)
+                    prc = Processor(filtered_frame)
 
-                    prc.gaussianFiltering((5, 5), 1.2)
-                    # prc.detectSkin(0, 35, 58, 173)
-                    # prc.connectedComponentLabeling()
-                    # prcImage = prc.src
-                    flow = prc.motionEstimation(prev_frame)
-                    vis_flow = prc.draw_flow(flow)
+                    prc.histEqulization()
+                    prc.detectSkin(0, 35, 58, 173)
+                    prc.connectedComponentLabeling()
+                    prcImage = prc.src
 
-                    # for i in range(frameHeight):
-                    #     for j in range(frameWidth):
-                    #         if prcImage[i, j, 0] == 0 and prcImage[i, j, 1] == 0 and prcImage[i, j, 2] == 0:
-                    #             frame[i, j] = (0, 0, 0)
+                    for i in range(frameHeight):
+                        for j in range(frameWidth):
+                            if prcImage[i, j, 0] == 0 and prcImage[i, j, 1] == 0 and prcImage[i, j, 2] == 255:
+                                frame[i, j] = (0, 0, 255)
 
-                    cv2.imshow('frame', vis_flow)
+                    cv2.imshow('frame', frame)
                     key = cv2.waitKey(frameRate)
 
                     if key == 27:
                         break
 
-                    prev_frame = frame
+                    # prev_frame = frame
+
+                cap.release()
+                cv2.destroyAllWindows()
+        else:
+            QMessageBox.information(self, 'QMessageBox', f"{fileName}이 존재하지 않습니다.")
+
+    def detectVehicle(self):
+        fileName = QFileDialog.getOpenFileName(self, 'Open File', '/', filter='Videos (*.avi *.mov *.mp4)')
+
+        if fileName[0]:
+            cap = cv2.VideoCapture(fileName[0])
+
+            if cap.isOpened():
+                frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                BG = np.zeros((frameHeight, frameWidth, 3))
+                cnt = 0
+
+                while cnt < 1000:
+                    retval, frame = cap.read()
+                    if not retval:
+                        break
+
+                    frame = cv2.GaussianBlur(frame, (0, 0), 1.2)
+                    BG += frame
+                    cnt += 1
+
+                BG = (BG / cnt).astype('uint8')
+                cap.release()
+                cv2.imshow('background image', BG)
+
+            cap = cv2.VideoCapture(fileName[0])
+
+            if cap.isOpened():
+                frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                frameRate = 30
+                tmpBG = np.zeros((frameHeight, frameWidth, 3))
+                retval, prev_frame = cap.read()
+                prev_frame = cv2.GaussianBlur(cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY), (0, 0), 1.2)
+                cnt = 0
+
+                while True:
+                    retval, frame = cap.read()
+                    if not retval:
+                        break
+
+                    filtered_frame = cv2.GaussianBlur(frame, (0, 0), 1.2)
+
+                    cnt += 1
+                    tmpBG += filtered_frame
+                    if cnt == 1000:
+                        BG = (tmpBG / cnt).astype('uint8')
+                        cnt = 0
+                        tmpBG = np.zeros((frameHeight, frameWidth, 3))
+
+                    prc = Processor(filtered_frame)
+
+                    sub = (prc.src.astype('int64') - BG.astype('int64'))
+                    sub[sub < 0] = 0
+                    sub = sub.astype('uint8')
+                    gray = cv2.cvtColor(sub, cv2.COLOR_BGR2GRAY)
+                    ret, sub = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+                    moving = (sub.astype('int64') - prev_frame.astype('int64'))
+                    moving[moving < 0] = 0
+                    moving = moving.astype('uint8')
+
+                    moving = cv2.cvtColor(moving, cv2.COLOR_GRAY2BGR)
+                    prc = Processor(moving)
+                    prc.connectedComponentLabeling_vehicle()
+
+                    for i in range(frameHeight):
+                        for j in range(frameWidth):
+                            if prc.src[i, j, 0] == 0 and prc.src[i, j, 1] == 0 and prc.src[i, j, 2] == 255:
+                                frame[i, j] = (0, 0, 255)
+
+                    cv2.imshow('frame', frame)
+                    key = cv2.waitKey(frameRate)
+
+                    prev_frame = sub
+
+                    if key == 27:
+                        break
 
                 cap.release()
                 cv2.destroyAllWindows()
@@ -83,5 +173,5 @@ class MyApp(QMainWindow):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = MyApp()
+    ex = Window()
     sys.exit(app.exec_())
--- processor.py
+++ processor.py
@@ -1,5 +1,5 @@
 import numpy as np
-from math import pi
+from math import pi, sqrt
 import cv2
 import collections
 
@@ -10,6 +10,15 @@ class Processor:
         self.Height = src.shape[0]
         self.Width = src.shape[1]
         self.Channel = src.shape[2]
+        self.colorList = [(255, 0, 0), (0, 255, 0), (255, 255, 0), (255, 0, 255), (0, 255, 255),
+                          (255, 255, 255), (128, 0, 0), (0, 128, 0), (0, 0, 128), (128, 128, 0),
+                          (128, 0, 128), (0, 128, 128), (75, 255, 32), (200, 100, 50), (80, 40, 222),
+                          (208, 94, 32), (102, 50, 255), (80, 50, 150), (10, 90, 222), (160, 30, 70),
+                          (48, 50, 99), (80, 110, 222), (111, 111, 222), (222, 30, 55), (70, 150, 160),
+                          (210, 50, 88), (99, 179, 99), (128, 60, 30), (30, 60, 128), (30, 128, 60),
+                          (128, 30, 60), (60, 128, 30), (60, 30, 128), (210, 88, 50), (88, 210, 50),
+                          (88, 50, 210), (50, 88, 210), (50, 210, 88), (222, 110, 80), (222, 80, 110),
+                          (110, 80, 222), (110, 222, 80), (150, 50, 80), (150, 80, 50), (50, 80, 150)]
 
     def gaussianFiltering(self, shape, sigma):
         dst = np.zeros((self.Height, self.Width, self.Channel), dtype='uint8')
@@ -29,32 +38,47 @@ class Processor:
 
         self.src = dst
 
-    def motionEstimation(self, prev):
+    def histEqulization(self):
+        gray = cv2.cvtColor(self.src, cv2.COLOR_BGR2GRAY)
+        height, width = gray.shape
+        histogram = np.zeros(256, dtype='int64')
+        after_histogram = np.zeros(256, dtype='int64')
+        sum_hist = np.zeros(256, dtype='int64')
+
+        for row in gray:
+            for pixel in row:
+                histogram[pixel] += 1
+
+        summ = 0.0
+        scale_factor = 255.0 / (width * height)
+        for i, h in enumerate(histogram):
+            summ += h
+            sum_hist[i] = (summ * scale_factor) + 0.5
+
+        for row in gray:
+            for pixel in row:
+                pixel = sum_hist[pixel]
+                after_histogram[pixel] += 1
+
+        hsv = cv2.cvtColor(self.src, cv2.COLOR_BGR2HSV)
+        h, w, c = hsv.shape
+        for i in range(0, h):
+            for j in range(0, w):
+                hsv[i][j][2] = gray[i][j]
+
+        self.src = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+
+    def motionEstimation(self, prev, step=16):
         now_frame = cv2.cvtColor(self.src, cv2.COLOR_BGR2GRAY)
         prev_frame = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
 
         flow = cv2.calcOpticalFlowFarneback(now_frame, prev_frame, None, 0.5, 3, 13, 3, 5, 1.1, 0)
-
-        return flow
-
-    def draw_flow(self, flow, step=16):
         gray = cv2.cvtColor(self.src, cv2.COLOR_BGR2GRAY)
         h, w = gray.shape[:2]
         y, x = np.mgrid[step / 2:h:step, step / 2:w:step].reshape(2, -1).astype(int)
         fx, fy = flow[y, x].T
-        lines = np.vstack([x, y, x + fx, y + fy]).T.reshape(-1, 2, 2)
-        lines = np.int32(lines + 0.5)
 
-        # 입력 영상의 컬러 영상 변환
-        vis = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
-
-        # 직선 그리기
-        cv2.polylines(vis, lines, 0, (0, 255, 255), lineType=cv2.LINE_AA)
-
-        for (x1, y1), (_x2, _y2) in lines:
-            cv2.circle(vis, (x1, y1), 1, (0, 128, 255), -1, lineType=cv2.LINE_AA)
-
-        return vis
+        return fx, fy
 
     def detectSkin(self, minh, maxh, mins, maxs):
         # yCrCb = cv2.cvtColor(frame, cv2.COLOR_RGB2YCrCb)
@@ -76,18 +100,14 @@ class Processor:
         gray = cv2.cvtColor(self.src, cv2.COLOR_BGR2GRAY)
         ret, otsu = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
-        kernel = np.ones((5, 5), dtype='uint8')
+        kernel = np.ones((3, 3), dtype='uint8')
         morp = cv2.dilate(cv2.erode(otsu, kernel, iterations=1), kernel, iterations=1)
 
-        pad_img = np.zeros((self.Height + 2, self.Width + 2), dtype='uint8')
-        pad_img[1:1 + self.Height, 1:1 + self.Width] = morp
+        pad_img = np.pad(morp, ((1, 1), (1, 1)), 'constant', constant_values=0)
         comp = np.zeros((self.Height + 2, self.Width + 2))
         cmpcnt = 0
         eqvTable = {}
-        cmplist = []
-        color_list = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255),
-                      (0, 255, 255), (128, 0, 0), (0, 128, 0), (0, 0, 128), (128, 128, 0),
-                      (128, 0, 128), (0, 128, 128)]
+        cmp_tmp_list = []
 
         for i in range(self.Height + 2):
             for j in range(self.Width + 2):
@@ -102,7 +122,7 @@ class Processor:
                     if pad_img[i - 1, j + 1] != 0 and not flag:
                         comp[i, j] = comp[i - 1, j + 1]
                         flag = True
-                    if comp[i, j - 1] != 0 and not flag:
+                    if pad_img[i, j - 1] != 0 and not flag:
                         comp[i, j] = comp[i, j - 1]
                         flag = True
 
@@ -118,6 +138,7 @@ class Processor:
                     if not flag:
                         cmpcnt += 1
                         comp[i][j] = cmpcnt
+                        cmp_tmp_list.append(HumanComponent(cmpcnt))
 
         orderedEqvTable = collections.OrderedDict(sorted(eqvTable.items(), reverse=True))
 
@@ -126,20 +147,222 @@ class Processor:
                 for j in range(self.Width + 2):
                     if comp[i, j] == key:
                         comp[i, j] = eqvTable[key]
+                        cmp_tmp_list = [cmp for cmp in cmp_tmp_list if cmp.num != key]
 
-        for i in range(self.Height + 2):
-            for j in range(self.Width + 2):
-                if comp[i, j] != 0 and cmplist.count(comp[i, j]) == 0:
-                    cmplist.append(comp[i, j])
+        base_size = (self.Width / 20) * (self.Height / 20)
+        base_size2 = (self.Width / 2) * (self.Height / 2)
+        cmp_list = []
+        for cmp in cmp_tmp_list:
+            loc = np.argwhere(comp == cmp.num)
+            cmp.setLocation(loc)
+
+            if base_size <= cmp.actual_area <= base_size2:
+                if 0.45 <= cmp.circularity <= 0.7:
+                    print(cmp.circularity)
+                    cmp_list.append(cmp)
 
         ret_img = np.zeros((self.Height, self.Width, 3), dtype='uint8')
 
         for i in range(self.Height):
             for j in range(self.Width):
                 cnt = 0
-                for cmpnum in cmplist[0:len(color_list)]:
-                    if comp[i + 1, j + 1] == cmpnum:
-                        ret_img[i, j] = color_list[cnt]
+                for cmp in cmp_list:
+                    if comp[i + 1, j + 1] == cmp.num:
+                        ret_img[i, j] = self.colorList[cnt]
                     cnt += 1
 
+        for cmp in cmp_list:
+            for i in range(cmp.xstart, cmp.xstop + 1):
+                ret_img[cmp.ystart, i] = (0, 0, 255)
+                ret_img[cmp.ystop, i] = (0, 0, 255)
+            for i in range(cmp.ystart + 1, cmp.ystop):
+                ret_img[i, cmp.xstart] = (0, 0, 255)
+                ret_img[i, cmp.xstop] = (0, 0, 255)
+
         self.src = ret_img
+
+    def connectedComponentLabeling_vehicle(self):
+        gray = cv2.cvtColor(self.src, cv2.COLOR_BGR2GRAY)
+
+        kernel = np.ones((7, 7), dtype='uint8')
+        morp = cv2.dilate(gray, kernel, iterations=1)
+
+        pad_img = np.pad(morp, ((1, 1), (1, 1)), 'constant', constant_values=0)
+        comp = np.zeros((self.Height + 2, self.Width + 2))
+        cmpcnt = 0
+        eqvTable = {}
+        cmp_tmp_list = []
+
+        for i in range(self.Height + 2):
+            for j in range(self.Width + 2):
+                if pad_img[i, j] != 0:
+                    flag = False
+                    if pad_img[i - 1, j - 1] != 0 and not flag:
+                        comp[i, j] = comp[i - 1, j - 1]
+                        flag = True
+                    if pad_img[i - 1, j] != 0 and not flag:
+                        comp[i, j] = comp[i - 1, j]
+                        flag = True
+                    if pad_img[i - 1, j + 1] != 0 and not flag:
+                        comp[i, j] = comp[i - 1, j + 1]
+                        flag = True
+                    if pad_img[i, j - 1] != 0 and not flag:
+                        comp[i, j] = comp[i, j - 1]
+                        flag = True
+
+                    if comp[i - 1, j - 1] > comp[i][j]:
+                        eqvTable[comp[i - 1, j - 1]] = comp[i][j]
+                    if comp[i - 1, j] > comp[i][j]:
+                        eqvTable[comp[i - 1, j]] = comp[i][j]
+                    if comp[i - 1, j + 1] > comp[i][j]:
+                        eqvTable[comp[i - 1, j + 1]] = comp[i][j]
+                    if comp[i, j - 1] > comp[i][j]:
+                        eqvTable[comp[i, j - 1]] = comp[i][j]
+
+                    if not flag:
+                        cmpcnt += 1
+                        comp[i][j] = cmpcnt
+                        cmp_tmp_list.append(VehicleComponent(cmpcnt))
+
+        orderedEqvTable = collections.OrderedDict(sorted(eqvTable.items(), reverse=True))
+
+        for key in orderedEqvTable.keys():
+            for i in range(self.Height + 2):
+                for j in range(self.Width + 2):
+                    if comp[i, j] == key:
+                        comp[i, j] = eqvTable[key]
+                        cmp_tmp_list = [cmp for cmp in cmp_tmp_list if cmp.num != key]
+
+        cmp_list = []
+        for cmp in cmp_tmp_list:
+            loc = np.argwhere(comp == cmp.num)
+            cmp.setLocation(loc)
+            if cmp.ratio >= 0.5 and cmp.convexity >= 0.45:
+                cmp_list.append(cmp)
+
+        ret_img = np.zeros((self.Height, self.Width, 3), dtype='uint8')
+
+        for i in range(self.Height):
+            for j in range(self.Width):
+                cnt = 0
+                for cmp in cmp_list:
+                    if cnt >= len(self.colorList):
+                        break
+                    if comp[i + 1, j + 1] == cmp.num:
+                        ret_img[i, j] = self.colorList[cnt]
+                    cnt += 1
+
+        for cmp in cmp_list:
+            for i in range(cmp.xstart, cmp.xstop + 1):
+                ret_img[cmp.ystart, i] = (0, 0, 255)
+                ret_img[cmp.ystop, i] = (0, 0, 255)
+            for i in range(cmp.ystart + 1, cmp.ystop):
+                ret_img[i, cmp.xstart] = (0, 0, 255)
+                ret_img[i, cmp.xstop] = (0, 0, 255)
+
+        self.src = ret_img
+
+
+class HumanComponent:
+    def __init__(self, num):
+        self.num = num
+        self.ystart = 0
+        self.xstart = 0
+        self.ystop = 0
+        self.xstop = 0
+        self.area = 0
+        self.actual_area = 0
+        self.circularity = 0
+
+    def setLocation(self, location):
+        (self.ystart, self.xstart), (self.ystop, self.xstop) = location.min(0) - 1, location.max(0) - 1
+
+        self.area = (self.ystop - self.ystart + 1) * (self.xstop - self.xstart + 1)
+
+        border = []
+        border_check_list = []
+        prev_loc = None
+
+        for i in range(self.xstart, self.xstop + 1):
+            border.append((self.ystart, i))
+        border_check_list.append(self.ystart)
+
+        for loc in location:
+            if loc[0] == self.ystart:
+                break
+            if border_check_list.count(loc[0]) == 0:
+                if prev_loc is not None:
+                    border.append(prev_loc)
+                border.append((loc[0], loc[1]))
+                border_check_list.append(loc[0])
+            prev_loc = (loc[0], loc[1])
+
+        for i in range(self.xstart, self.xstop + 1):
+            border.append((self.ystop, i))
+
+        max_diam = 0
+        for i in range(0, len(border)):
+            for j in range(i, len(border)):
+                diam = sqrt((border[i][0] - border[j][0]) ** 2 + (border[i][1] - border[j][1]) ** 2)
+                if diam > max_diam:
+                    max_diam = diam
+
+        self.actual_area = location.size
+        self.circularity = (4 * self.area) / (pi * max_diam ** 2)
+
+
+class VehicleComponent:
+    def __init__(self, num):
+        self.num = num
+        self.ystart = 0
+        self.xstart = 0
+        self.ystop = 0
+        self.xstop = 0
+        self.convexity = 0
+        self.ratio = 0
+
+    def setLocation(self, location):
+        (self.ystart, self.xstart), (self.ystop, self.xstop) = location.min(0) - 1, location.max(0) - 1
+
+        border = []
+        border_check_list = []
+        prev_loc = None
+
+        for i in range(self.xstart, self.xstop + 1):
+            border.append((self.ystart, i))
+        border_check_list.append(self.ystart)
+
+        for loc in location:
+            if loc[0] == self.ystart:
+                break
+            if border_check_list.count(loc[0]) == 0:
+                if prev_loc is not None:
+                    border.append(prev_loc)
+                border.append((loc[0], loc[1]))
+                border_check_list.append(loc[0])
+            prev_loc = (loc[0], loc[1])
+
+        for i in range(self.xstart, self.xstop + 1):
+            border.append((self.ystop, i))
+
+        convex_perimeter = 0
+        for b in border:
+            if (b[0] - 1, b[1]) in border:
+                convex_perimeter += 1
+            if (b[0], b[1] - 1) in border:
+                convex_perimeter += 1
+            if (b[0] + 1, b[1]) in border:
+                convex_perimeter += 1
+            if (b[0], b[1] + 1) in border:
+                convex_perimeter += 1
+
+        xlen = self.xstop - self.xstart
+        ylen = self.ystop - self.ystart
+        perimeter = (xlen + ylen) * 2
+
+        self.convexity = perimeter / convex_perimeter
+
+        if ylen > xlen:
+            self.ratio = (self.xstop - self.xstart) / (self.ystop - self.ystart)
+        else:
+            self.ratio = (self.ystop - self.ystart) / (self.xstop - self.xstart)
